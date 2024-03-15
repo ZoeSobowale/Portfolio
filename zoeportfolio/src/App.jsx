57dc1774022aa926d2b149d98861fd0dc6e96368
ZoeSobowale <38224967+ZoeSobowale@users.noreply.github.com>
2024-03-15 22:39:31 +0000

    update

--- zoeportfolio/src/App.jsx
+++ zoeportfolio/src/App.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
